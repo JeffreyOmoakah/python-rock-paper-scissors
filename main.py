0b178b41c2c3f6d04c947633f8bf0a035f9f8d78
Theynvyjeffrey <jomoakah3@gmail.com>
2024-02-22 01:24:53 +0100

    Fixes bugs

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
+# Import the random module to generate random choices:
 import random
 
-possible_choice = ["Rock", "Scissor", "Paper"]
-user_choice = input("Choose Rock, Paper, or Scissors: ")
-computer_choice = random.choice(possible_choice)
+# Create a list of possible choices for the game:
+possible_choices = ["Rock", "Scissors", "Paper"]  # Note: Corrected typo in "Scissors"
 
+# Prompt the user to enter their choice:
+user_choice = input("Choose Rock, Paper, or Scissors: ")
+
+# Get the computer's choice randomly from the possible choices:
+computer_choice = random.choice(possible_choices)
+
+# Print the user's choice and the computer's choice:
 print("Your choice: ", user_choice)
 print("Computer choice: ", computer_choice)
 
+# Determine the winner based on the rules of Rock, Paper, Scissors:
 if user_choice == computer_choice:
     print("It's a tie!")
-elif user_choice == "Rock" and computer_choice == "Scissor":
-    print("You win")
+elif user_choice == "Rock" and computer_choice == "Scissors":
+    print("You win!")  # Exclamation mark added for excitement
 elif user_choice == "Paper" and computer_choice == "Rock":
-    print("You win")
-elif user_choice == "Scissor" and computer_choice == "Paper":
-    print("You win")
+    print("You win!")  # Exclamation mark added for excitement
+elif user_choice == "Scissors" and computer_choice == "Paper":
+    print("You win!")  # Exclamation mark added for excitement
 else:
     print("Computer wins!")
